0aed2a14ccd5e6e1dd8aab1b56ffafbb3a128aac
Usman Hafeez <usmanhafeez147@gmail.com>
2019-02-07 15:46:31 +0500

    header changes

--- src/app/services/global-search.service.ts
+++ src/app/services/global-search.service.ts
@@ -6,9 +6,14 @@ import { Injectable, Output, EventEmitter} from '@angular/core';
 export class GlobalSearchService {
   searchFilterValue: any;
   @Output() globalSearch = new EventEmitter<Event>();
+  @Output() deviceInfo = new EventEmitter<Event>();
   constructor() { }
 
   globalSearchFunc(value){
     this.globalSearch.emit(value);
+  }
+
+  editDevice(device){
+    this.deviceInfo.emit(device);
   }
 }
