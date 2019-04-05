3962972a694f515d2aceb077ee4bb8d189721cc8
HamzaDawood <hamza.dawood007@gmail.com>
2019-04-05 10:58:45 +0500

    change name of email to account email

--- src/routes/devices/components/editDevice.js
+++ src/routes/devices/components/editDevice.js
@@ -41,7 +41,7 @@ export default class EditDealer extends Component {
             's_dealer': this.state.s_dealer,
             'expiry_date': this.state.expiry_date,
             'start_date': this.state.start_date,
-            'email': this.state.email,
+            'account_email': this.state.email,
             'name': this.state.name,
             'model': this.state.model,
             'sttatus': this.state.status,
