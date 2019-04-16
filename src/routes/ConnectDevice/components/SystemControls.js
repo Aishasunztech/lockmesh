84f51a79a2537ce9dc7a1e25d35ed274882bf2e8
Adeel <adeel@sunztech.com>
2019-04-16 17:43:10 +0500

    make System Controls page

--- src/routes/ConnectDevice/components/SystemControls.js
+++ src/routes/ConnectDevice/components/SystemControls.js
@@ -1,19 +1,78 @@
-import React, { Component, Fragment } from 'react';
-import { Card, Row, Col, List, Button, message } from "antd";
-
+import React, { Component, Fragment, Typography } from 'react';
+import { List, Switch, Col, Row } from "antd";
 
 export default class SystemControls extends Component {
   render() {
-    const mainItem1 = [
-     
-    ]
     return (
       <Fragment>
-        <List
-          className="dev_main_menu"
-          size="small"
-          dataSource={mainItem1}
-        />
+        <div>
+          <List>
+            <div className="row width_100 m-0 sec_head1">
+              <div className="col-md-4 col-sm-4 col-xs-4 p-0 text-center">
+                <span>Guest</span>
+                <Switch defaultChecked size="small" />
+              </div>
+              <div className="col-md-4 col-sm-4 col-xs-4 p-0 text-center">
+                <span>Encrypt</span>
+                <Switch size="small" />
+              </div>
+              <div className="col-md-4 col-sm-4 col-xs-4 p-0 text-center">
+                <span>Enable</span>
+                <Switch defaultChecked size="small" />
+              </div>
+            </div>
+            <List.Item>
+              <div className="row width_100">
+                <div className="col-md-10 col-sm-10 col-xs-10">
+                  <span>Wifi</span>
+                </div>
+                <div className="col-md-2 col-sm-2 col-xs-2">
+                  <Switch defaultChecked size="small" />
+                </div>
+              </div>
+            </List.Item>
+            <List.Item>
+              <div className="row width_100">
+                <div className="col-md-10 col-sm-10 col-xs-10">
+                  <span>Bluetooth</span>
+                </div>
+                <div className="col-md-2 col-sm-2 col-xs-2">
+                  <Switch size="small" />
+                </div>
+              </div>
+            </List.Item>
+            <List.Item>
+              <div className="row width_100">
+                <div className="col-md-10 col-sm-10 col-xs-10">
+                  <span>Hotspot</span>
+                </div>
+                <div className="col-md-2 col-sm-2 col-xs-2">
+                  <Switch defaultChecked size="small" />
+                </div>
+              </div>
+            </List.Item>
+            <List.Item>
+              <div className="row width_100">
+                <div className="col-md-10 col-sm-10 col-xs-10">
+                  <span>Location Services</span>
+                </div>
+                <div className="col-md-2 col-sm-2 col-xs-2">
+                  <Switch size="small" />
+                </div>
+              </div>
+            </List.Item>
+            <List.Item>
+              <div className="row width_100">
+                <div className="col-md-10 col-sm-10 col-xs-10">
+                  <span>Block Calls</span>
+                </div>
+                <div className="col-md-2 col-sm-2 col-xs-2">
+                  <Switch defaultChecked size="small" />
+                </div>
+              </div>
+            </List.Item>
+          </List>
+        </div>
       </Fragment>
     )
   }
