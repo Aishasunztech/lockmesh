37714bfa1f267ce91c55861b1b0a92918db8ed23
usmanhafeez147 <usmanhafeez147@gmail.com>
2019-04-24 12:13:59 +0500

    topbar

--- src/containers/App/MainApp.js
+++ src/containers/App/MainApp.js
@@ -3,13 +3,13 @@ import {Layout} from "antd";
 
 import Sidebar from "../Sidebar/index";
 
-// import HorizontalDefault from "../Topbar/HorizontalDefault/index";
-// import HorizontalDark from "../Topbar/HorizontalDark/index";
-// import InsideHeader from "../Topbar/InsideHeader/index";
-// import AboveHeader from "../Topbar/AboveHeader/index";
-// import BelowHeader from "../Topbar/BelowHeader/index";
-// import Topbar from "../Topbar/index";
-// import Customizer from "routes/myProfile/components/Customizer";
+import HorizontalDefault from "../Topbar/HorizontalDefault/index";
+import HorizontalDark from "../Topbar/HorizontalDark/index";
+import InsideHeader from "../Topbar/InsideHeader/index";
+import AboveHeader from "../Topbar/AboveHeader/index";
+import BelowHeader from "../Topbar/BelowHeader/index";
+import Topbar from "../Topbar/index";
+import Customizer from "routes/myProfile/components/Customizer";
 
 import {footerText} from "util/config";
 import App from "routes/index";
@@ -49,32 +49,32 @@ export class MainApp extends Component {
         return '';
     }
   };
-  // getNavStyles = (navStyle) => {
-  //   switch (navStyle) {
-  //     case NAV_STYLE_DEFAULT_HORIZONTAL :
-  //       return <HorizontalDefault/>;
-  //     case NAV_STYLE_DARK_HORIZONTAL :
-  //       return <HorizontalDark/>;
-  //     case NAV_STYLE_INSIDE_HEADER_HORIZONTAL :
-  //       return <InsideHeader/>;
-  //     case NAV_STYLE_ABOVE_HEADER :
-  //       return <AboveHeader/>;
-  //     case NAV_STYLE_BELOW_HEADER :
-  //       return <BelowHeader/>;
-  //     case NAV_STYLE_FIXED :
-  //       return <Topbar/>;
-  //     case NAV_STYLE_DRAWER :
-  //       return <Topbar/>;
-  //     case NAV_STYLE_MINI_SIDEBAR :
-  //       return <Topbar/>;
-  //     case NAV_STYLE_NO_HEADER_MINI_SIDEBAR :
-  //       return <NoHeaderNotification/>;
-  //     case NAV_STYLE_NO_HEADER_EXPANDED_SIDEBAR :
-  //       return <NoHeaderNotification/>;
-  //     default :
-  //       return null;
-  //   }
-  // };
+  getNavStyles = (navStyle) => {
+    switch (navStyle) {
+      case NAV_STYLE_DEFAULT_HORIZONTAL :
+        return <HorizontalDefault/>;
+      case NAV_STYLE_DARK_HORIZONTAL :
+        return <HorizontalDark/>;
+      case NAV_STYLE_INSIDE_HEADER_HORIZONTAL :
+        return <InsideHeader/>;
+      case NAV_STYLE_ABOVE_HEADER :
+        return <AboveHeader/>;
+      case NAV_STYLE_BELOW_HEADER :
+        return <BelowHeader/>;
+      case NAV_STYLE_FIXED :
+        return <Topbar/>;
+      case NAV_STYLE_DRAWER :
+        return <Topbar/>;
+      case NAV_STYLE_MINI_SIDEBAR :
+        return <Topbar/>;
+      case NAV_STYLE_NO_HEADER_MINI_SIDEBAR :
+        return <NoHeaderNotification/>;
+      case NAV_STYLE_NO_HEADER_EXPANDED_SIDEBAR :
+        return <NoHeaderNotification/>;
+      default :
+        return null;
+    }
+  };
 
   getSidebar = (navStyle, width) => {
     if (width < TAB_SIZE) {
@@ -103,7 +103,7 @@ export class MainApp extends Component {
       <Layout className="gx-app-layout">
         {this.getSidebar(navStyle, width)}
         <Layout>
-          {/* {this.getNavStyles(navStyle)} */}
+          {this.getNavStyles(navStyle)}
           <Content className={`gx-layout-content ${ this.getContainerClass(navStyle)} `}>
             <App match={match}/>
             <Footer>
