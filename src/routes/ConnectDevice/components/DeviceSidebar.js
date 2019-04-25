86ca148a4a62fc4fcd73a92f81e49f691416f524
usmanhafeez147 <usmanhafeez147@gmail.com>
2019-04-25 12:29:22 +0500

    column setting on connect device page

--- src/routes/ConnectDevice/components/DeviceSidebar.js
+++ src/routes/ConnectDevice/components/DeviceSidebar.js
@@ -2,7 +2,37 @@ import React, { Component } from 'react'
 import styles from './AppList';
 import { Card, Table, Icon } from "antd";
 import { getStatus, getColor, checkValue } from '../../../routes/utils/commonUtils'
+import {
+    DEVICE_ID,
+    DEVICE_REMAINING_DAYS,
+    DEVICE_FLAGGED,
+    DEVICE_STATUS,
+    DEVICE_MODE,
+    DEVICE_NAME,
+    DEVICE_ACTIVATION_CODE,
+    DEVICE_ACCOUNT_EMAIL,
+    DEVICE_PGP_EMAIL,
+    DEVICE_CHAT_ID,
+    DEVICE_CLIENT_ID,
+    DEVICE_DEALER_ID,
+    DEVICE_DEALER_PIN,
+    DEVICE_MAC_ADDRESS,
+    DEVICE_SIM_ID,
+    DEVICE_IMEI_1,
+    DEVICE_SIM_1,
+    DEVICE_IMEI_2,
+    DEVICE_SIM_2,
+    DEVICE_SERIAL_NUMBER,
+    DEVICE_MODEL,
+    DEVICE_START_DATE,
+    DEVICE_EXPIRY_DATE,
+    DEVICE_DEALER_NAME,
+    DEVICE_S_DEALER,
+    DEVICE_S_DEALER_NAME
+} from '../../../constants/DeviceConstants';
+
 let make_red = 'captilize';
+
 export default class DeviceSidebar extends Component {
     // constructor(props){
     //     super(props);
@@ -35,18 +65,13 @@ export default class DeviceSidebar extends Component {
 
         return [
             {
-                key: 1,
-                name: (<a href="javascript:void(0)">Name:</a>),
-                value: (<span className="captilize">{device_details.name}</span>)
-            },
-            {
                 key: 10,
-                name: (<a href="javascript:void(0)" >Status:</a>),
+                name: (<a href="javascript:void(0)" >{DEVICE_STATUS}:</a>),
                 value: <span style={color}>{device_details.finalStatus}</span>,
             },
             {
                 key: 171,
-                name: (<a href="javascript:void(0)">Mode:</a>),
+                name: (<a href="javascript:void(0)">{DEVICE_MODE}:</a>),
                 value: device_details.online ? (device_details.online == "On") ? (<span style={{ color: "green" }}>Online</span>) : (<span style={{ color: "red" }}>Offline</span>) : "N/A"
             },
             {
@@ -55,13 +80,18 @@ export default class DeviceSidebar extends Component {
                 value: (device_details.flagged === '') ? "Not Flagged" : device_details.flagged
             },
             {
-                key: 101,
-                name: (<a href="javascript:void(0)" >Device Name:</a>),
-                value: checkValue(device_details.name)
+                key: 1,
+                name: (<a href="javascript:void(0)">{DEVICE_NAME}:</a>),
+                value: (<span className="captilize">{device_details.name}</span>)
             },
+            // {
+            //     key: 101,
+            //     name: (<a href="javascript:void(0)" >Device Name:</a>),
+            //     value: checkValue(device_details.name)
+            // },
             {
                 key: 2,
-                name: (<a href="javascript:void(0)">Account Email:</a>),
+                name: (<a href="javascript:void(0)">{DEVICE_ACCOUNT_EMAIL}:</a>),
                 value: checkValue(device_details.account_email)
             },
 
@@ -72,7 +102,7 @@ export default class DeviceSidebar extends Component {
             },
             {
                 key: 812,
-                name: (<a href="javascript:void(0)">Activation Code:</a>),
+                name: (<a href="javascript:void(0)">{DEVICE_ACTIVATION_CODE}:</a>),
                 value: checkValue(device_details.activation_code)
             },
             {
@@ -92,18 +122,17 @@ export default class DeviceSidebar extends Component {
             },
             {
                 key: 7,
-                name: (<a href="javascript:void(0)">Dealer Name:</a>),
+                name: (<a href="javascript:void(0)">{DEVICE_DEALER_NAME}:</a>),
                 value: (<span className="captilize">{checkValue(device_details.dealer_name)}</span>)
             },
             {
                 key: 8,
-                name: (<a href="javascript:void(0)">Dealer Pin:</a>),
+                name: (<a href="javascript:void(0)">{DEVICE_DEALER_PIN}:</a>),
                 value: checkValue(device_details.link_code)
             },
-
             {
                 key: 9,
-                name: (<a href="javascript:void(0)">Mac Address:</a>),
+                name: (<a href="javascript:void(0)">{DEVICE_MAC_ADDRESS}:</a>),
                 value: checkValue(device_details.mac_address)
             },
             {
@@ -115,33 +144,33 @@ export default class DeviceSidebar extends Component {
 
             {
                 key: 13,
-                name: (<a href="javascript:void(0)">IMEI 1:</a>),
+                name: (<a href="javascript:void(0)">{DEVICE_IMEI_1}:</a>),
                 value: checkValue(device_details.imei)
             },
             {
                 key: 14,
-                name: (<a href="javascript:void(0)">SIM 1:</a>),
+                name: (<a href="javascript:void(0)">{DEVICE_SIM_1}:</a>),
                 value: checkValue(device_details.simno)
             },
             {
                 key: 15,
-                name: (<a href="javascript:void(0)">IMEI 2:</a>),
+                name: (<a href="javascript:void(0)">{DEVICE_IMEI_2}:</a>),
                 value: checkValue(device_details.imei2)
             },
             {
                 key: 16,
-                name: (<a href="javascript:void(0)">SIM 2:</a>),
+                name: (<a href="javascript:void(0)">{DEVICE_SIM_2}:</a>),
                 value: checkValue(device_details.simno2)
             },
 
             {
                 key: 111,
-                name: (<a href="javascript:void(0)">Serial Number:</a>),
+                name: (<a href="javascript:void(0)">{DEVICE_SERIAL_NUMBER}:</a>),
                 value: checkValue(device_details.serial_number)
             },
             {
                 key: 11,
-                name: (<a href="javascript:void(0)">Model:</a>),
+                name: (<a href="javascript:void(0)">{DEVICE_MODEL}:</a>),
                 value: checkValue(device_details.model)
             },
             {
@@ -162,13 +191,13 @@ export default class DeviceSidebar extends Component {
             },
             {
                 key: 18,
-                name: (<a href="javascript:void(0)">Start Date:</a>),
+                name: (<a href="javascript:void(0)">{DEVICE_START_DATE}:</a>),
                 // value: (Date(device_details.start_date,'mm-dd-Y'))
                 value: checkValue(device_details.start_date)
             },
             {
                 key: 19,
-                name: (<a href="javascript:void(0)">Expiry Date:</a>),
+                name: (<a href="javascript:void(0)">{DEVICE_EXPIRY_DATE}:</a>),
                 value: checkValue(device_details.expiry_date)
             }
         ]
