390eafb2d81860c6e43d29fa4e93917faa4afcc7
HamzaDawood <hamza.dawood007@gmail.com>
2019-04-29 12:49:35 +0500

    real time user add in users list

--- src/components/AppFilter/index.js
+++ src/components/AppFilter/index.js
@@ -72,7 +72,7 @@ class AppFilter extends Component {
     }
 
     handlePagination(value) {
-        //   alert(value);
+        console.log(value);
         this.setState({ DisplayPages: value })
         this.props.handlePagination(value);
     }
