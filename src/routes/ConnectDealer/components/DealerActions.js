38a8eab64ef39211a803899ed01c2b99722f04e5
Usman <usmanhafeez147@gmail.com>
2019-12-03 10:53:26 +0500

    dealer search fixing

--- src/routes/ConnectDealer/components/DealerActions.js
+++ src/routes/ConnectDealer/components/DealerActions.js
@@ -117,7 +117,7 @@ export default class DealerAction extends Component {
         console.log(dealerList);
 
         states.dealerList = dealerList;
-        // states.searchedValue = value
+        states.searchedValue = value
 
         if (index === -1) {
             states.disabledSearchButton = true;
