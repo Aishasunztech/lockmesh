d9b497b7b547a84485bbd774116a41b601cd24f3
Muhammad Bilal <bilal@sunztech.com>
2020-01-24 19:08:19 +0500

    fixed minor bugs of support app

--- src/routes/Support/Tickets/components/TicketDetail/index.js
+++ src/routes/Support/Tickets/components/TicketDetail/index.js
@@ -36,46 +36,63 @@ class TicketDetail extends React.Component {
           <div className="gx-mail-detail-inner">
             <div className="gx-mail-header">
               <div className="gx-mail-header-content gx-col gx-pl-0">
-                <div className="gx-subject" style={{fontSize: '15px'}}>
-                  <span style={{display: 'block', float: 'left', marginRight: '10px'}}><i className="icon icon-arrow-left gx-icon-btn" onClick={() => {
+                <div className="gx-subject font15">
+                  <span className="display-float right10"><i className="icon icon-arrow-left gx-icon-btn" onClick={() => {
                     updateState({ currentMail: null });
                     this.props.updateOnTicketPage(false);
                     this.props.resetCurrentTicket();
                   }} />
                   </span>
-                  <span style={{display: 'block', float: 'left'}}>Subject: {supportTicket.subject}<br />
+                  <span className="display-float">Subject: {supportTicket.subject}<br />
                     Ticket Id: ({ supportTicket.ticketId })</span>
                 </div>
+                {/*<div className="top10">*/}
+                  <div className="float-left" >
+                    {supportTicket.status === 'open' && closeSupportTicketStatus === false?
+                      <Button type="primary" size="small" onClick={() => {
+                        this.addReply();
+                      }}>Add Reply</Button>
+                      : ''
+                    }
+
+                    {supportTicket.status === 'open' && closeSupportTicketStatus === false?
+                      <Button type="danger" size="small" onClick={() => {
+                        onCloseTicket(supportTicket);
+                      }}>Close This Ticket</Button>
+                      : ''
+                    }
+                  </div>
+                {/*</div>*/}
 
               </div>
 
-              <div className="gx-mail-header-actions">
+              {/*<div className="gx-mail-header-actions">*/}
 
-                <div onClick={() => {
-                  this.addReply();
-                }}>
-                  {supportTicket.status === 'open' && closeSupportTicketStatus === false?
-                    <Button type="primary" size="small">Add Reply</Button>
-                    : ''
-                  }
-                </div>
-              </div>
-              <div className="gx-mail-header-actions">
+                {/*<div onClick={() => {*/}
+                  {/*this.addReply();*/}
+                {/*}}>*/}
+                  {/*{supportTicket.status === 'open' && closeSupportTicketStatus === false?*/}
+                    {/*<Button type="primary" size="small">Add Reply</Button>*/}
+                    {/*: ''*/}
+                  {/*}*/}
+                {/*</div>*/}
+              {/*</div>*/}
+              {/*<div className="gx-mail-header-actions">*/}
 
-                <div onClick={() => {
-                  onCloseTicket(supportTicket);
-                }}>
-                  {supportTicket.status === 'open' && closeSupportTicketStatus === false?
-                    <Button type="danger" size="small">Close This Ticket</Button>
-                    : ''
-                  }
-                </div>
-              </div>
+                {/*<div onClick={() => {*/}
+                  {/*onCloseTicket(supportTicket);*/}
+                {/*}}>*/}
+                  {/*{supportTicket.status === 'open' && closeSupportTicketStatus === false?*/}
+                    {/*<Button type="danger" size="small">Close This Ticket</Button>*/}
+                    {/*: ''*/}
+                  {/*}*/}
+                {/*</div>*/}
+              {/*</div>*/}
             </div>
 
-            <hr/>
+            {/*<hr/>*/}
 
-            <p style={{fontSize: '16px', textAlign: 'justify'}}>
+            <p className="font16 justify top15">
               <strong>Description:</strong><br />
 
               {supportTicket.description}
