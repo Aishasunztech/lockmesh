6fc5c8b678fa5f4517914dd84ba816dd9f521fed
Muhammad Bilal <bilal@sunztech.com>
2020-02-07 10:41:41 +0500

    Merge branch 'development' of http://51.75.68.188/secure/lockmesh into support_chat

--- src/appRedux/services/RestServices.js
+++ src/appRedux/services/RestServices.js
@@ -6,7 +6,7 @@ import SupportSystemSocketIO from "socket.io-client";
 axios.interceptors.response.use(function (response) {
   // Any status code that lie within the range of 2xx cause this function to trigger
   // Do something with response data
-  console.log(response);
+//   console.log(response);
   return response;
 }, function (error) {
   // Any status codes that falls outside the range of 2xx cause this function to trigger
